57b3ea38c4a6f0e1925bbc4a31341fb0845937c3
smThilshan <smthilshan@gmail.com>
2025-06-23 14:30:51 +0400

    added configurations

--- lib/utils/config.dart
+++ lib/utils/config.dart
@@ -6,4 +6,52 @@ class Config {
   static double? screenWidth;
 
   // width and height initialization
+  void init(BuildContext context) {
+    mediaQueryData = MediaQuery.of(context);
+    screenHeight = mediaQueryData!.size.height;
+    screenWidth = mediaQueryData!.size.width;
+  }
+
+  // spacing height
+  static get widthSize {
+    return screenWidth;
+  }
+
+  static get heightSize {
+    return screenHeight;
+  }
+
+  static const spaceSmall = SizedBox(
+    height: 25,
+  );
+
+  static final spaceMedium = SizedBox(
+    height: screenHeight! * 0.05,
+  );
+
+  static final spaceLarge = SizedBox(
+    height: screenHeight! * 0.08,
+  );
+
+  // textformfield border
+
+  static const outlinedBorder = OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(8)),
+  );
+
+  static const focusBorder = OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(8)),
+    borderSide: BorderSide(
+      color: Colors.greenAccent,
+    ),
+  );
+
+  static const errorBorder = OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(8)),
+    borderSide: BorderSide(
+      color: Colors.red,
+    ),
+  );
+
+  static const primaryColor = Color(0xFF4CAF50);
 }
---
+++ lib/utils/text.dart
@@ -0,0 +1,12 @@
+class AppText {
+  static final enText = {
+    'appName': "Doctor App",
+    'signIn_text': 'Sign In to your account',
+    'welcome_text': 'Welcome',
+    'registered_text':
+        'You can easily sign up, and connect to the Doctors nearby you',
+    'signUp_text': 'Don\'t have an account?',
+    'social_login': "Sign in with social accounts",
+    'forgot_password': 'Forgot Password?',
+  };
+}
