67f1e8cc6f35397d102d2a4944c78d83a4fe57b9
smThilshan <smthilshan@gmail.com>
2025-06-23 14:14:38 +0400

    initial commit

---
+++ lib/utils/config.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Config {
+  static MediaQueryData? mediaQueryData;
+  static double? screenHeight;
+  static double? screenWidth;
+
+  // width and height initialization
+}
